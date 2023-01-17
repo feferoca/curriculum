26b0cbb84842766741d12ef02bfd0386af2e353a
feferoca <122050212+feferoca@users.noreply.github.com>
2023-01-17 11:48:47 -0300

    Add files via upload

---
+++ micv.js
@@ -0,0 +1,24 @@
+    
+    function presentacion(){
+        Element =document.getElementById("pres");
+    }
+    
+    function personal() {
+    Element =document.getElementById("pers");
+    
+    }
+   
+    function enAmarillo2() {
+        Element =document.getElementById("labo");
+    
+    }
+   
+    function enAmarillo3() {
+        Element =document.getElementById("habi");
+        
+    }
+  
+    function enAmarillo4() {
+        Element =document.getElementById("estu");
+        
+    }
